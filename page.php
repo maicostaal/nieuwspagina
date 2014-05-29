a65f7ced4d718c2842557316588a62e1f1fe39d8
Coen de Zeeuw <coendezeeuw@mbp-van-coen.lan>
2014-05-29 10:36:08 +0200

    first commit

---
+++ page.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Nieuwspagina - Gemaakt door Maico</title>
+<link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+
+</head>
+<body style="font:16px 'Ubuntu', Verdana, Geneva, sans-serif;">
+
+<div style="width:500px; margin:0 auto;">
+<h1 style="text-align:center;">Onderhoudbare Nieuwspagina</h1>
+
+
+    <div class="article">
+        <h3>Nieuwstitel</h3>
+        <span class="date">29-05-2014</span>
+        <div>Dit is een nieuwsbericht.</div>
+    </div>
+
+    <div class="article">
+        <h3>Nieuwstitel</h3>
+        <span class="date">29-05-2014</span>
+        <div>Dit is een nieuwsbericht.</div>
+    </div>
+
+</p>
+</div>
+</body>
+</html>
