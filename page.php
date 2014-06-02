6676c1062e40d601acaefb23f22f051234bbe466
maicostaal <maicostaal@gmail.com>
2014-06-02 22:37:27 +0200

    Form verwijderd want deze moet op de administratie-pagina

--- page.php
+++ page.php
@@ -21,14 +21,6 @@ mysql_select_db($datb) or die ("Er is iets mis gegaan");
 
 <div style="width:500px; margin:0 auto;">
     <h1 style="text-align:center;">Onderhoudbare Nieuwspagina</h1>
-
-    <form method="post" action=""><p>
-            <span class="label">Titel van het bericht:</span><br />
-            <input type="text" style="width:478px; padding:5px 10px; margin:0; -webkit-box-shadow: 0px 2px 5px rgba(50, 50, 50, 0.5); -moz-box-shadow: 0px 2px 5px rgba(50, 50, 50, 0.5); box-shadow:0px 2px 5px rgba(50, 50, 50, 0.5); border:1px solid #000" name="naam" maxlength="16" /><br /><br />
-            <span class="label">Inhoud van het bericht:</span><br />
-            <textarea style="width:478px; height:250px; padding:5px 10px; margin:0; -webkit-box-shadow: 0px 2px 5px rgba(50, 50, 50, 0.5); -moz-box-shadow: 0px 2px 5px rgba(50, 50, 50, 0.5); box-shadow:0px 2px 5px rgba(50, 50, 50, 0.5); border:1px solid #000" name="bericht" id="tekst" rows="6" cols="37"></textarea><br /><br />
-            <input class="input"  type="submit" value="Toevoegen" onclick="this.value='Nieuwsbericht wordt geplaatst...';" /> <input class="input"  type="reset" value="Herstel" />
-        </p></form>
     <hr />
     <p>
 
